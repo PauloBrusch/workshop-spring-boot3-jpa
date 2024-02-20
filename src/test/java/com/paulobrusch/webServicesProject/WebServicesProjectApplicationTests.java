c913f2ac20c7d62ffb9c883f42b94455a19f6dfb
Paulo Brusch <contato.paulobrusch@gmail.com>
2024-02-20 00:41:16 -0300

    first commit

---
+++ src/test/java/com/paulobrusch/webServicesProject/WebServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulobrusch.webServicesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
