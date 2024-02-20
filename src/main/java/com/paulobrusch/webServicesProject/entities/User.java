ffc603824c09413b133e744e99ffc310addbf17b
Paulo Brusch <contato.paulobrusch@gmail.com>
2024-02-20 01:56:23 -0300

    H2 database

--- src/main/java/com/paulobrusch/webServicesProject/entities/User.java
+++ src/main/java/com/paulobrusch/webServicesProject/entities/User.java
@@ -1,10 +1,19 @@
 package com.paulobrusch.webServicesProject.entities;
 
+import jakarta.persistence.*;
+
+
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
 
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
