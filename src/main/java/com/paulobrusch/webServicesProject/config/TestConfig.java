cf11f4cfcdcf3729595ae6be5fefb4f5cc1d5b80
Paulo Brusch <contato.paulobrusch@gmail.com>
2024-02-23 04:33:22 -0300

    Product

--- src/main/java/com/paulobrusch/webServicesProject/config/TestConfig.java
+++ src/main/java/com/paulobrusch/webServicesProject/config/TestConfig.java
@@ -4,8 +4,10 @@ import com.paulobrusch.webServicesProject.entities.Category;
 import com.paulobrusch.webServicesProject.entities.Order;
 import com.paulobrusch.webServicesProject.entities.User;
 import com.paulobrusch.webServicesProject.entities.enums.OrderStatus;
+import com.paulobrusch.webServicesProject.entities.enums.Product;
 import com.paulobrusch.webServicesProject.repositories.CategoryRepository;
 import com.paulobrusch.webServicesProject.repositories.OrderRepository;
+import com.paulobrusch.webServicesProject.repositories.ProductRepository;
 import com.paulobrusch.webServicesProject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -44,8 +49,15 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2,cat3));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     }
 }
---
+++ src/main/java/com/paulobrusch/webServicesProject/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.paulobrusch.webServicesProject.repositories;
+
+import com.paulobrusch.webServicesProject.entities.enums.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
